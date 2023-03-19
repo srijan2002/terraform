375d24721c237d870ad93994ee08d464f8a35984
Srijan Majumdar <srjnmajumdar8@gmail.com>
2023-03-19 12:32:04 +0530

    Changes production

--- prod.tf
+++ prod.tf
@@ -14,140 +14,140 @@ provider "aws" {
   secret_key = ""
 }
 
-# resource "aws_vpc" "vpc" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-#   tags = {
-#     Name = "VPC"
-#   }
-# }
+  tags = {
+    Name = "VPC"
+  }
+}
 
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.vpc.id
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
 
-#   tags = {
-#     Name = "GW"
-#   }
-# }
+  tags = {
+    Name = "GW"
+  }
+}
 
-# resource "aws_route_table" "route_table" {
-#   vpc_id = aws_vpc.vpc.id
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
-#   route {
-#     ipv6_cidr_block        = "::/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
-#   tags = {
-#     Name = "Route Table"
-#   }
-# }
+  tags = {
+    Name = "Route Table"
+  }
+}
 
-# resource "aws_subnet" "subnet" {
-#   vpc_id     = aws_vpc.vpc.id
-#   cidr_block = "10.0.1.0/24"
-#   availability_zone = "us-east-1a"
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
-#   tags = {
-#     Name = "Subnet"
-#   }
-# }
+  tags = {
+    Name = "Subnet"
+  }
+}
 
-# resource "aws_route_table_association" "a" {
-#   subnet_id      = aws_subnet.subnet.id
-#   route_table_id = aws_route_table.route_table.id
-# }
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
 
-# resource "aws_security_group" "allow_web" {
-#   name        = "allow_web_traffic"
-#   description = "Allow Web inbound traffic"
-#   vpc_id      = aws_vpc.vpc.id
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
 
-#   ingress {
-#     description      = "HTTPS"
-#     from_port        = 443
-#     to_port          = 443
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
     
-#   }
+  }
 
-#   ingress {
-#     description      = "HTTP"
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
    
-#   }
+  }
 
-#   ingress {
-#     description      = "SSH"
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
     
-#   }
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   tags = {
-#     Name = "allow_web"
-#   }
-# }
+  tags = {
+    Name = "allow_web"
+  }
+}
 
-# resource "aws_network_interface" "network" {
-#   subnet_id       = aws_subnet.subnet.id
-#   private_ips     = ["10.0.1.50"]
-#   security_groups = [aws_security_group.allow_web.id]
+resource "aws_network_interface" "network" {
+  subnet_id       = aws_subnet.subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
 
-# }
+}
 
-# resource "aws_eip" "eip" {
-#   vpc                       = true
-#   network_interface         = aws_network_interface.network.id
-#   associate_with_private_ip = "10.0.1.50"
-#   depends_on = [aws_internet_gateway.gw,aws_instance.ubuntu]
-# }
+resource "aws_eip" "eip" {
+  vpc                       = true
+  network_interface         = aws_network_interface.network.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.gw,aws_instance.ubuntu]
+}
 
-# resource "aws_instance" "ubuntu" {
-#   ami = "ami-0263e4deb427da90e"
-#   instance_type = "t2.micro"
-#   availability_zone = "us-east-1a"
-#   key_name = "terraform"
+resource "aws_instance" "ubuntu" {
+  ami = "ami-0263e4deb427da90e"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "terraform"
 
-#   network_interface {
-#     device_index = 0
-#     network_interface_id = aws_network_interface.network.id
-#   }
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.network.id
+  }
 
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               sudo apt update -y
-#               mkdir new_folder
-#               EOF
-#    tags = {
-#     Name = "Server"
-#    }
-# }
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              mkdir new_folder
+              EOF
+   tags = {
+    Name = "Server"
+   }
+}
 
-# output "public_ip" {
-#   value = aws_eip.eip.public_ip
-# }
+output "public_ip" {
+  value = aws_eip.eip.public_ip
+}
 
-# output "instance_type" {
-#   value = aws_instance.ubuntu.instance_type
-# }
+output "instance_type" {
+  value = aws_instance.ubuntu.instance_type
+}
